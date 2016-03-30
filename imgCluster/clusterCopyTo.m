65d4c5680a1e779a77295cb18b9cb94e8e40c6a4
h005 <hejingwuuestc@163.com>
2016-03-30 20:25:47 +0800

    modify meanshift clustering

---
+++ imgCluster/clusterCopyTo.m
@@ -0,0 +1,17 @@
+%% copy each cluster imgs into one folder named as clusterID
+function clusterCopyTo(imgDir,clusterDir,fileName,cluster)
+% imgDir contains img files listed in fileName
+% clusterDir is the dest folder which will contain clusters named with
+% clueterID
+num = max(cluster);
+for i=1:num
+    ind = find(cluster == i);
+    if exist([clusterDir '/' num2str(i)]) == 7
+        rmdir([clusterDir '/' num2str(i)],'s');        
+    end
+    mkdir([clusterDir '/' num2str(i)]);
+    % copy files into it
+    for j=1:length(ind)
+        copyfile([imgDir '/' fileName{j}],[clusterDir '/' num2str(i) '/']);
+    end
+end
