b2934e5570e6735122ac3b6ae0f6d3ec46203dfa
h005 <hejingwuuestc@163.com>
2016-09-08 15:59:50 +0800

    merge 201609081559

---
+++ vanishfea/clearFiles.m
@@ -0,0 +1,25 @@
+%% this scripte was created to clear .vnf .2dvnf .2dvnfname files
+% clearpath1 '/home/h005/Documents/vpDataSet/tools/vpData/model/vpFea/model.suffix'
+% clearpath2 '/home/h005/Documents/vpDataSet/model/vpFea/model.suffix'
+
+function clearFiles(modelList,suffix)
+
+for i=1:numel(modelList)
+    for j = 1:numel(suffix)
+        clearpath1 = ['/home/h005/Documents/vpDataSet/tools/vpData/' modelList{i} '/vpFea/' modelList{i} suffix{j}];
+        clearpath2 = ['/home/h005/Documents/vpDataSet/' modelList{i} '/vpFea/' modelList{i} suffix{j}];
+        
+        if exist(clearpath1,'file') == 2
+            delete(clearpath1);
+        end
+        if exist(clearpath2,'file') == 2
+            delete(clearpath2);
+        end
+        
+    end
+end
+
+
+
+
+end
