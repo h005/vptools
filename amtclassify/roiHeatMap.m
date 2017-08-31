dc1af4e31af1a1c63096ad85a958dd8c22fb256b
h005 <hejingwuuestc@163.com>
2017-08-31 15:30:18 +0800

    show the roi heat map, please genearte the roi features with our 3DfeatureCheck project first

---
+++ amtclassify/roiHeatMap.m
@@ -0,0 +1,8 @@
+%% show surfaceRegionInterest
+
+model = 'model13';
+virtualModel = {model,'circle'};
+
+ps = load(['/home/h005/Documents/vpDataSet/tools/vpData/' model '/vpFea/' model '.roi']);
+showColorMapMirror;
+% showColorMap;
